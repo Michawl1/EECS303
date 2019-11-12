c327eee3e60a241fb0916fa0286472bd8f927066
Michael Thompson <mjt106@case.edu>
2019-11-12 11:19:02 -0500

    adding kernel

---
+++ LabK/blink_kernal.c
@@ -0,0 +1,54 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/gpio.h>
+#include <linux/delay.h>
+
+MODULE_LICENSE("BeerWare");
+MODULE_AUTHOR("Michael Thompson, Joshua Schlitling");
+MODULE_DESCRIPTION("A LED blink kernal module");
+MODULE_VERSION("1.0");
+
+#define LED_PIN	21
+
+#define HIGH 1
+#define LOW 0
+
+static int __init initialization(void)
+{
+	int result = 0;
+
+	if(!gpio_is_valid(LED_PIN))
+	{
+		printk(KERN_INFO "blink_km: Initialization failed. Invalid LED GPIO.\n");
+		return -ENODEV;
+	}
+
+	gpio_request(LED_PIN, "sysfs");
+
+	gpio_direction_output(LED_PIN, HIGH);
+	gpio_export(LED_PIN, false);
+
+	int i;
+	for(i = 0; i < 10; i++)
+	{
+		gpio_set_value(LED_PIN, HIGH);
+		mdelay(1000);
+		gpio_set_value(LED_PIN, LOW);
+		mdelay(1000);
+	}
+
+	printk(KERN_INFO "blink_kernal: Initialization succeeded.");
+	return result;
+}
+
+static void __exit finalization(void)
+{
+	gpio_set_value(LED_PIN, LOW);
+	gpio_unexport(LED_PIN):
+
+	printk(KERN_INFO "blink_kernal: Exit.\n");
+}
+
+module_init(initialization);
+module_exit(finalization);
